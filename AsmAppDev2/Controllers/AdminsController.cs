65cf13c68c017256cb658d7cd8d8af01d4d258a2
mrbjn99 <58242504+mrbjn99@users.noreply.github.com>
2020-10-26 06:25:11 +0700

    Update AdminsController.cs

--- AsmAppDev2/Controllers/AdminsController.cs
+++ AsmAppDev2/Controllers/AdminsController.cs
@@ -136,7 +136,7 @@ namespace AsmAppDev2.Controllers
                 return HttpNotFound();
             }
 
-            //Khai báo biến userId thuộc Curent.User.Identity và truy cập vào trường Id thông qua GetUserId       
+      
             var userId = System.Web.HttpContext.Current.User.Identity.GetUserId();
             userId = accountInDb.Id;
             if (userId != null)
